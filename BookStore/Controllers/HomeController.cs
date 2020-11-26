bf619b86788eda878e4aca79f49f9b74a672887e
Nicoleta Ciobanu <ci.nicoletta@gmail.com>
2020-11-26 21:56:55 +0200

    Initial commit Base project structure

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BookViewModel>> Index()
+        {
+            var result = new List<BookViewModel>();
+            BookViewModel book1 = new BookViewModel()
+            {
+                Author = "Jhon Doe",
+                Name = "Basic book"
+            };
+            result.Add(book1);
+            
+            return Ok(result);
+        }
+    }
+}
---
+++ BookStore/Models/BookViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Models
+{
+    public class BookViewModel
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool InStock { get; set; }
+    }
+}
